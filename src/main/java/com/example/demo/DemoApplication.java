b59b45a391b3d6d6385f07eae88c4545c16b943f
jacky9911 <89850425+jacky9911@users.noreply.github.com>
2021-09-12 19:35:50 +0800

    second commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,7 +16,7 @@ public class DemoApplication {
 	}
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "Docker") String name) {
+	public String hello(@RequestParam(value = "name", defaultValue = "Dockers") String name) {
 		return String.format("Hello %s!", name);
 	}
 
